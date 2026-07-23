325be5971e7b17849065361731bb5537b4694b93
Abhijeet singh <abhijeetsingh7752@gmail.com>
2026-07-23 11:53:58 +0530

    initial commit

---
+++ LAB1/Hello.js
@@ -0,0 +1,14 @@
+// console.log("Abhijeet singh")
+
+//write a function to take two number and return there sum
+function sum(a,b){
+    return a+b;
+}
+
+const add=(a,b)=>{
+    return a+b;
+};
+const add2 =(a,b)=> a+b;
+console.log(sum(20,10))
+console.log(add(25, 10));
+console.log(add2(60, 10));
